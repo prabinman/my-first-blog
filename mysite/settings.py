c03b36f178d6502c5e1f937b1a1cd8d373c23108
prabinman <shrestha.prabinman@gmail.com>
2016-03-21 10:03:38 +0545

    Added security to the site through authorization.

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,6 +116,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Login_redirect_url
+LOGIN_REDIRECT_URL = '/'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
 2. Import the include() function: from django.conf.urls import url, include
 3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url, include
+from django.conf.urls import patterns, url, include
 from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
-]
+urlpatterns = patterns('',
+                       url(r'^admin/', admin.site.urls),
+                       url(r'^accounts/login/$',
+                           'django.contrib.auth.views.login'),
+                       url(r'^accounts/logout/$',
+                           'django.contrib.auth.views.logout',
+                           {'next_page': '/'}),
+                       url(r'', include('blog.urls')),
+                       )
